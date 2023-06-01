2405c224abea34e0f9d178c694b498c290dc6557
neddington <neddington@cascadia.edu>
2023-06-01 10:45:38 -0700

    Sample Data Updated

--- CoreDataRelationships/DataController.swift
+++ CoreDataRelationships/DataController.swift
@@ -36,127 +36,93 @@ class DataController : ObservableObject {
         session2.name = "Hogwarts Head Master/Mistress Session"
         session2.date = Date()
         
-        let interview = Interview(context: viewContext)
-        interview.id = UUID()
-        interview.name = "Potions Interview"
-        interview.session = session1
+        let interview1 = Interview(context: viewContext)
+        interview1.id = UUID()
+        interview1.name = "Potions Interview"
+        interview1.session = session1
+        
+        let interview2 = Interview(context: viewContext)
+        interview2.id = UUID()
+        interview2.name = "Defense Against Dark Arts Interview"
+        interview2.session = session1
         
         
         let question1 = Question(context: viewContext)
         question1.id = UUID()
-        question1.name = "Minerva McGonagall"
+        question1.name = "What sparked your interest about working at Hogwarts?"
         question1.date = Date()
-        question1.interview = interview
+        question1.interview = interview1
         question1.session = session1
         
         let question2 = Question(context: viewContext)
         question2.id = UUID()
-        question2.name = "Severus Snape"
+        question2.name = "What would you say the most peculular thing is about the Wizarding World?"
         question2.date = Date()
-        question2.interview = interview
+        question2.interview = interview1
         question2.session = session2
         
         let question3 = Question(context: viewContext)
         question3.id = UUID()
-        question3.name = "Pomona Sprout"
+        question3.name = "What is your experience working with Defense against the dark arts?"
         question3.date = Date()
-        question3.interview = interview
+        question3.interview = interview1
         question3.session = session1
         
         let question4 = Question(context: viewContext)
         question4.id = UUID()
-        question4.name = "Rubeus Hagrid"
+        question4.name = "What uniqueness do you believe you'd bring to Hogwarts?"
         question4.date = Date()
-        question4.interview = interview
+        question4.interview = interview1
         question4.session = session1
         
         let question5 = Question(context: viewContext)
         question5.id = UUID()
-        question5.name = "Gilderoy Lockhart"
+        question5.name = "What do you know about the history of the school and how it came to be?"
         question5.date = Date()
-        question5.interview = interview
+        question5.interview = interview2
         question5.session = session1
         
         let response1 = Response(context: viewContext)
         response1.id = UUID()
-        response1.name = "Hermione Granger"
+        response1.name = "The fact that the castle is always moving and theres always something different to see."
         response1.date = Date()
         response1.session = session2
-        response1.interview = interview
-        response1.question = [question1, question2, question3, question4, question5]
+        response1.interview = interview1
+        response1.question = [question1]
         
         let response2 = Response(context: viewContext)
         response2.id = UUID()
-        response2.name = "Percy Weasley"
+        response2.name = "All the intricate creatures you won't find anywhere else"
         response1.date = Date()
         response1.session = session2
-        response1.interview = interview
-        response1.question = [question1, question2, question3, question4, question5]
+        response1.interview = interview1
+        response1.question = [question2]
         
         
         let response3 = Response(context: viewContext)
         response3.id = UUID()
-        response3.name = "Neville Longbottom"
+        response3.name = "Only what my mother taught me."
         response3.date = Date()
         response3.session = session2
-        response3.interview = interview
-        response3.question = [question1, question2, question3, question4, question5]
+        response3.interview = interview1
+        response3.question = [question3]
         
         let response4 = Response(context: viewContext)
         response4.id = UUID()
-        response4.name = "Harry Potter"
+        response4.name = "I'm strange so I should fit right in!"
         response4.date = Date()
         response4.session = session1
-        response4.interview = interview
-        response4.question = [question1, question2, question3, question4, question5]
+        response4.interview = interview1
+        response4.question = [question4]
         
         let response5 = Response(context: viewContext)
         response5.id = UUID()
-        response5.name = "Ginny Weasley"
+        response5.name = "Only from what I've learned from reaching, watching and playing Harry Potter."
         response5.date = Date()
         response5.session = session2
-        response5.interview = interview
-        response5.question = [question1, question2, question3, question4, question5]
+        response5.interview = interview1
+        response5.question = [question5]
         
-        let response6 = Response(context: viewContext)
-        response6.id = UUID()
-        response6.name = "Draco Malfoy"
-        response6.date = Date()
-        response6.session = session1
-        response6.interview = interview
-        response6.question = [question1, question2, question3, question4, question5]
-        
-        let response7 = Response(context: viewContext)
-        response7.id = UUID()
-        response7.name = "Ron Weasley"
-        response7.date = Date()
-        response7.session = session2
-        response7.interview = interview
-        response7.question = [question1, question2, question3, question4, question5]
-        
-        let response8 = Response(context: viewContext)
-        response8.id = UUID()
-        response8.name = "Luna Lovegood"
-        response8.date = Date()
-        response8.session = session1
-        response8.interview = interview
-        response8.question = [question1, question2, question3, question4, question5]
-        
-        let response9 = Response(context: viewContext)
-        response9.id = UUID()
-        response9.name = "Cho Chang"
-        response9.date = Date()
-        response9.session = session2
-        response9.interview = interview
-        response9.question = [question1, question2, question3, question4, question5]
-        
-        let response10 = Response(context: viewContext)
-        response10.id = UUID()
-        response10.name = "Parvati Patil"
-        response10.date = Date()
-        response10.session = session1
-        response10.interview = interview
-        response10.question = [question1, question2, question3, question4, question5]
         
         do {
             try viewContext.save()
